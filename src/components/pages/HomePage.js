425e43b429b850ae5d75a681ab0895dceed40e05
Ruben Zaldanan <rooben.zaldana@gmail.com>
2022-02-17 10:56:03 -0500

    product grid setup

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -5,7 +5,43 @@ const HomePage = (props) => {
 
     return (
         <Layout color="red">
-          <h1>This is the homepage</h1>
+            <Box p={4}>
+                {productData.map(product => (
+                    <Box
+                        key={product.id}
+                        mb={4}
+                        display="flex"
+                        alignItems="center"
+                    >
+                        <Card sx={{ mx: 'auto', maxWidth: 800, maxHeight: 500 }} style={{ paddingTop: '10px' }}>
+                            <CardHeader
+                                action={
+                                    <Typography>${product.price / 100}</Typography>
+                                }
+                                title={product.title}
+                                subheader={product.brand}
+                            />
+                            <CardMedia
+                                component="img"
+                                height="194"
+                                image={product.image}
+                                alt="Paella dish"
+                            />
+                            <CardContent>
+                                <Typography variant="body2" color="text.secondary">
+                                    {product.description}
+                                </Typography>
+                            </CardContent>
+                            <CardActions disableSpacing>
+                                <Button variant="text">Add to cart</Button>
+                                <IconButton aria-label="add to favorites" sx={{ marginLeft: 'auto' }}>
+                                    <FavoriteIcon />
+                                </IconButton>
+                            </CardActions>
+                        </Card>
+                    </Box>
+                ))}
+            </Box>
         </Layout>
     )
 };
